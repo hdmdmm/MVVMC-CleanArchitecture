6a57a2b2f97e79281a4ba2b19a047c76cbf36a7f
Dmitry Khotyanovich <dmitrykhotyanovich@gmail.com>
2022-08-30 15:59:26 +0200

    Initial Commit

---
+++ MVVMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMC
+//
+//  Created by Dmitry Kh on 30.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ MVVMCUITests/MVVMCUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVMCUITestsLaunchTests.swift
+//  MVVMCUITests
+//
+//  Created by Dmitry Kh on 30.08.22.
+//
+
+import XCTest
+
+class MVVMCUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
